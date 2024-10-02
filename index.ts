da5b6f6b3ee84f01066d5bba9136eecb7ca1bc6c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-10-02 12:10:24 +0200

    ref: externalized getCustomers to /functions/getCustomers

--- index.ts
+++ index.ts
@@ -7,45 +7,7 @@ import { Invoice, RawInvoices } from './typings/Invoice'
 import { RawStockChange, StockChange } from './typings/StockChange'
 import { getInvoices } from '@/functions/invoices/getInvoices'
 import parseInvoice from '@/functions/invoices/parseInvoice'
-
-export async function getCustomers(limit: number = -1) {
-  let customers: Array<Customer> = []
-  let pastRetrieval = 0
-  do {
-    let queryAmount = limit - customers.length
-
-    if (limit === -1) queryAmount = 1000
-    else if (queryAmount > 1000) queryAmount = 1000
-
-    const { users } = await GET<RawCustomers>('users', [`limit=${queryAmount}`])
-
-    customers.push(
-      ...users.map(
-        (u): Customer => ({
-          id: u.user_id,
-          timestamp: u.user_timestamp ? new Date(Date.parse(u.user_timestamp)) : null,
-          city: u.user_city,
-          country: u.user_country,
-          postCode: u.user_postalCode,
-          email: u.user_email ?? '',
-          firstName: u.user_firstname ?? '',
-          lastName: u.user_surname ?? '',
-          street: u.user_street,
-          houseNumber: u.user_houseNumber ?? '',
-          phone: u.user_phoneNumber ?? '',
-          salutation: u.user_salutation ?? '',
-          birthday: u.user_birthday ?? '',
-          notes: u?.user_notes?.replace(/(?:\\r)+/g, '')?.split('\n') ?? null,
-          company: u?.user_company,
-          uid_number: u?.user_uidNumber,
-        }),
-      ),
-    )
-    pastRetrieval = users.length
-  } while (customers.length < limit && pastRetrieval > 0)
-
-  return customers
-}
+import getCustomers from '@/functions/customers/getCustomers'
 
 /**
  * This function returns the buy-history for given array of article-ids.
