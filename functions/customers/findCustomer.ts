cfd119f56f52aa745b1b15cd724e339338af8d9c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-10-02 12:12:06 +0200

    ref: externalized findCustomer to /functions/customers

---
+++ functions/customers/findCustomer.ts
@@ -0,0 +1,33 @@
+import { Customer } from '@/typings/Customer'
+import getCustomers from '@/functions/customers/getCustomers'
+
+/**
+ * This function looks for duplicate users based on the given user. In doing so it checks exact matches, partial address matches and partial name matches.
+ * @param user The user-data that is being checked for duplicates.
+ * @returns An array of users that match or partially match the given user-data.
+ */
+export default async function findCustomer(user: Partial<Customer>) {
+  const customers = await getCustomers()
+
+  const exactMatches = customers.filter((customer) =>
+    Object.keys(user)
+      .map((_key) => {
+        const key = _key as keyof Customer
+        return customer[key] === user[key]
+      })
+      .every((el) => el),
+  )
+
+  const partialAddressMatches = customers.filter(
+    (customer) =>
+      customer.country?.trim() === user.country?.trim() &&
+      customer.city?.trim() === user.city?.trim() &&
+      customer.postCode?.trim() === user.postCode?.trim() &&
+      customer.street?.trim() === user.street?.trim() &&
+      customer.houseNumber?.trim() === user.houseNumber?.trim(),
+  )
+  const partialNameMatches = customers.filter((customer) => customer.firstName?.trim() === user.firstName?.trim() && customer.lastName?.trim() === user.lastName?.trim())
+
+  //? Combine all matches and remove duplicates
+  return [...exactMatches, ...partialAddressMatches, ...partialNameMatches].filter((v, i, a) => a.indexOf(v) === i)
+}
--- index.ts
+++ index.ts
@@ -7,7 +7,6 @@ import { Invoice, RawInvoices } from './typings/Invoice'
 import { RawStockChange, StockChange } from './typings/StockChange'
 import { getInvoices } from '@/functions/invoices/getInvoices'
 import parseInvoice from '@/functions/invoices/parseInvoice'
-import getCustomers from '@/functions/customers/getCustomers'
 
 /**
  * This function returns the buy-history for given array of article-ids.
@@ -28,37 +27,6 @@ export async function getBuyHistory(ids: Array<Article['id']>) {
   })
 
   return history
-}
-
-/**
- * This function looks for duplicate users based on the given user. In doing so it checks exact matches, partial address matches and partial name matches.
- * @param user The user-data that is being checked for duplicates.
- * @returns An array of users that match or partially match the given user-data.
- */
-export async function findCustomer(user: Partial<Customer>) {
-  const customers = await getCustomers()
-
-  const exactMatches = customers.filter((customer) =>
-    Object.keys(user)
-      .map((_key) => {
-        const key = _key as keyof Customer
-        return customer[key] === user[key]
-      })
-      .every((el) => el),
-  )
-
-  const partialAddressMatches = customers.filter(
-    (customer) =>
-      customer.country?.trim() === user.country?.trim() &&
-      customer.city?.trim() === user.city?.trim() &&
-      customer.postCode?.trim() === user.postCode?.trim() &&
-      customer.street?.trim() === user.street?.trim() &&
-      customer.houseNumber?.trim() === user.houseNumber?.trim(),
-  )
-  const partialNameMatches = customers.filter((customer) => customer.firstName?.trim() === user.firstName?.trim() && customer.lastName?.trim() === user.lastName?.trim())
-
-  //? Combine all matches and remove duplicates
-  return [...exactMatches, ...partialAddressMatches, ...partialNameMatches].filter((v, i, a) => a.indexOf(v) === i)
 }
 
 export async function createUser(customer: Partial<Omit<Customer, 'timestamp' | 'id'>>): Promise<RawCustomers['users'][number]> {
