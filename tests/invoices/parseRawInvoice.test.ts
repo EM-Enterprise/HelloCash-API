6a5c58efcab9500b5df3b177e5704221cdf58d95
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-27 15:37:13 +0100

    ref: removed todo test case

--- tests/invoices/parseRawInvoice.test.ts
+++ tests/invoices/parseRawInvoice.test.ts
@@ -3,19 +3,6 @@ import { ZodError } from 'zod'
 import { InvoiceSchema, RawInvoice } from '@/schemas/Invoice'
 import parseRawInvoice from '@/functions/invoices/parseRawInvoice'
 
-describe('parseInvoices - ', () => {
-  it.todo('check passing valid invoice object, implement Zod-Schemas to validate invoice objects inside of parseInvoice function')
-})
-
-// test('parseInvoices - check passing invalid invoice object', () => {
-//   const invoice = {} as RawInvoices['invoices'][number]
-//
-//   // todo: Implemented Zod-Schemas to validate invoice objects inside of parseInvoice function
-//
-//   expect(parseInvoice(invoice)).rejects.toThrow(TypeError)
-//   expect(parseInvoice(invoice)).rejects.toThrow('Passed invoice object has not the required properties.')
-// })
-
 const dummyRawInvoice: RawInvoice = {
   invoice_id: '1',
   invoice_number: '123',
