9d643c15a63db87da5a1f699783d91c069cdf015
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-28 16:36:58 +0100

    test: mocked GET function in findInvoiceById test case

--- tests/invoices/findInvoiceById.test.ts
+++ tests/invoices/findInvoiceById.test.ts
@@ -2,8 +2,13 @@ import { getInvoices } from '@/functions/invoices/getInvoices'
 import * as dotenv from 'dotenv'
 import { setAuthorization } from '@/config/authorization'
 import findInvoiceById from '@/functions/invoices/findInvoiceById'
+import schemaDefaults from '@/schemas/SchemaDefaults'
+import { RawInvoicesSchema } from '@/schemas/Invoice'
 
 dotenv.config()
+
+const mockedGet = jest.requireActual('@/api/GET')
+jest.spyOn(mockedGet, 'default').mockImplementation(() => Promise.resolve(schemaDefaults(RawInvoicesSchema)))
 
 beforeEach(() => {
   setAuthorization(process.env.AUTH_TOKEN!)
@@ -12,6 +17,8 @@ beforeEach(() => {
 test('findInvoiceById - check for finding correct invoice', async () => {
   const invoices = await getInvoices(1)
   const fondInvoice = await findInvoiceById(invoices[0].id)
+
   expect(invoices.length).toBeGreaterThan(0)
   expect(fondInvoice).toEqual(invoices[0])
+  expect(mockedGet.default).toHaveBeenCalledTimes(1)
 })
