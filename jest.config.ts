c3a781992ce70b4c96f71ec5ad22def74b8b44f1
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-29 16:31:23 +0100

    chore: added jest.setup to initialize dotenv.config
    This jest.setup file is run before each test-suite.

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const config: Config.InitialOptions = {
   testEnvironment: 'node',
   workerThreads: true,
   testTimeout: 10000,
-  setupFilesAfterEnv: ['./jest.mocks.ts'],
+  setupFilesAfterEnv: ['./jest.setup.ts', './jest.mocks.ts'],
   // Other Jest configurations...
 
   testPathIgnorePatterns: ['dist'],
