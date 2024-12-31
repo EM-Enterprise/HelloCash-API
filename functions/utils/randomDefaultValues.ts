acd28492bc54196bd38c0fedf7badeb5687807eb
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-31 12:23:28 +0100

    chore: excluded internal module from documentation

--- functions/utils/randomDefaultValues.ts
+++ functions/utils/randomDefaultValues.ts
@@ -1,10 +1,16 @@
 const randomNumber = Math.random() * 100
 const randomNumberAsString = (Math.random() * 100).toFixed(0)
 
+/**
+ * @internal
+ */
 export function getRandomNumber() {
   return +randomNumber.toFixed(0)
 }
 
+/**
+ * @internal
+ */
 export function getRandomNumberAsString() {
   return randomNumber.toFixed(0)
 }
