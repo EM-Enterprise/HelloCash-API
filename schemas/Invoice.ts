cd224e1cd2f1d5cc93adbf4407b187e055735f08
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-01 15:46:29 +0100

    feat: updated Invoice schema, types and utils

--- schemas/Invoice.ts
+++ schemas/Invoice.ts
@@ -1,13 +1,14 @@
 import { z } from 'zod'
 import { CustomerSchema } from '@/schemas/customer/Customer'
 import { getRandomNumber, getRandomNumberAsString } from '@/functions/utils/randomDefaultValues'
-import { stripZodDefault } from '@/schemas/utils/stripZodDefaultValues'
 import { InvoiceItemSchema } from '@/schemas/invoice/InvoiceItem'
+import { useSchema } from '@/schemas/utils/useSchema'
 
 /**
+ * This schema defines the structure of an invoice including default values
  * @internal
  */
-export const InvoiceSchema_DefaultValues = z.object({
+export const InvoiceSchema = z.object({
   system_id: z.unknown().default(getRandomNumberAsString()),
   id: z.number().default(getRandomNumber()),
   timestamp: z.string().default(new Date(Date.parse('2024/12/27')).toISOString()),
@@ -17,6 +18,8 @@ export const InvoiceSchema_DefaultValues = z.object({
   customer: CustomerSchema.optional(),
   items: z.array(InvoiceItemSchema),
 })
-export const InvoiceSchema = stripZodDefault(InvoiceSchema_DefaultValues)
 
 export type Invoice = z.infer<typeof InvoiceSchema>
+
+const { validateObject: validateInvoice, getDummyObject: getDummyInvoice, safeParseObject: safeParseInvoice } = useSchema<Invoice>(InvoiceSchema)
+export { validateInvoice, getDummyInvoice, safeParseInvoice }
