32fe62cad3065a065f0fa040680790ca3f56d2f7
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-01 16:00:59 +0100

    feat: externalized RawArticleCategory schema, types and utils

--- schemas/ArticleCategory.ts
+++ schemas/ArticleCategory.ts
@@ -1,14 +1,9 @@
 import { z } from 'zod'
+import { RawArticleCategorySchema } from '@/schemas/article-category/RawArticleCategory'
 
 export const ArticleCategorySchema = z.object({
   id: z.number(),
   name: z.string(),
-})
-
-export const RawArticleCategorySchema = z.object({
-  article_category_id: z.number(),
-  article_parent_category_id: z.number(),
-  article_category_name: z.string(),
 })
 
 export const RawArticleCategoriesSchema = z.object({
@@ -17,5 +12,4 @@ export const RawArticleCategoriesSchema = z.object({
 })
 
 export type ArticleCategory = z.infer<typeof ArticleCategorySchema>
-export type RawArticleCategory = z.infer<typeof RawArticleCategorySchema>
 export type RawArticleCategories = z.infer<typeof RawArticleCategoriesSchema>
---
+++ schemas/article-category/RawArticleCategory.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import { StripZodDefault } from '@/schemas/utils/stripZodDefaultValues'
+import { useSchema } from '@/schemas/utils/useSchema'
+
+/**
+ * This schema defines the structure of a raw-article-category object including default values.
+ * @internal
+ */
+export const RawArticleCategorySchema = z.object({
+  article_category_id: z.number(),
+  article_parent_category_id: z.number(),
+  article_category_name: z.string(),
+})
+
+export type RawArticleCategory = z.infer<StripZodDefault<typeof RawArticleCategorySchema>>
+
+const { validateObject: validateRawArticleCategory, getDummyObject: getDummyRawArticleCategory, safeParseObject: safeParseRawArticleCategory } = useSchema<RawArticleCategory>(RawArticleCategorySchema)
+export { validateRawArticleCategory, getDummyRawArticleCategory, safeParseRawArticleCategory }
