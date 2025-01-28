79dd24e1f4b9c75ffa57a6d28e899483f1a83a2f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-28 09:05:27 +0100

    feat: exported Hellocash class within root/index.ts
    This class provides consumers with all the functions from within the /functions directory when initializing a new Hellocash instance.

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { setAuthorization } from '@/config/authorization'
+
+import * as invoiceFunctions from '@/functions/invoices/_export'
+import * as customerFunctions from '@/functions/customers/_export'
+import * as articleFunctions from '@/functions/articles/_export'
+import * as categoryFunctions from '@/functions/categories/_export'
+
+type InvoiceFunctionType = typeof invoiceFunctions
+type CustomerFunctionType = typeof customerFunctions
+type ArticleFunctionType = typeof articleFunctions
+type CategoryFunctionType = typeof categoryFunctions
+
+interface Hellocash extends InvoiceFunctionType, CustomerFunctionType, ArticleFunctionType, CategoryFunctionType {}
+
+class Hellocash {
+  constructor(authorizationToken: string) {
+    setAuthorization(authorizationToken)
+
+    Object.assign(this, invoiceFunctions)
+    Object.assign(this, customerFunctions)
+    Object.assign(this, articleFunctions)
+    Object.assign(this, categoryFunctions)
+  }
+}
+
+export default Hellocash
